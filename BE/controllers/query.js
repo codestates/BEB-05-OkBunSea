49c5a1e3b5b74221edaf12ceb7efc9dde4e31cc6
donggni0712 <donggni0712@naver.com>
2022-08-08 22:13:22 +0900

    :bug:Fix Error About Cors

--- BE/controllers/query.js
+++ BE/controllers/query.js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
-
+    console.log(req)
     const body = req.body
     res.status(200);
     res.send(`buy : ${JSON.stringify(body)}`);
--- BE/index.js
+++ BE/index.js
@@ -5,6 +5,13 @@ const app = express();
 const controllers = require("./controllers");
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type,X-Requested-With");
+  next();
+});
+
 app.post("/query", controllers.query);
 app.post("/mint", controllers.mint);
 app.post("/buy", controllers.buy);
