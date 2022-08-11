28513ab94af4fdb3aae8d5b31a6037eec4558f37
dhjefflee <lexwoodtrans@gmail.com>
2022-08-11 09:44:16 +0900

    front end modified

--- FE/src/page/about.js
+++ FE/src/page/about.js
@@ -1,12 +1,49 @@
 import React from 'react';
-// import axios from 'axios';
-// import { Button, Card,Form } from 'react-bootstrap';
+import temp from './img/temp.jpg';
+import team1 from './img/temp.jpg';
+import team2 from './img/temp.jpg';
+import team3 from './img/temp.jpg';
+import team4 from './img/temp.jpg';
 
-
-
-export default function About() {
+export default function About () {
     return (
-        <h1>About</h1>
+        <div class="container">
+        <div class="text-center">
+            <h2 class="p-4 mb-3 section-heading text-uppercase">Our Amazing Team</h2>
+            <h3 class="mb-3 section-subheading text-primary">BEB 05 OKBUNSEA</h3>
+        </div><br/> <br/>
+        <div class="row"> 
+            <div class="col-lg-3">
+                <div class="team-member">
+                    <img class="p-2 mx-auto rounded-circle" src= {temp} alt="..." />
+                    <h4>강 인 국</h4>
+                    <p class="text-muted">Back End (SmartContract)</p>                    
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="team-member">
+                <img class="p-2 mx-auto rounded-circle" src= {temp} alt="..." />
+                    <h4>오 동 재</h4>
+                    <p class="text-muted">Server, DB</p>                    
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="team-member">
+                <img class="p-2 mx-auto rounded-circle" src= {temp} alt="..." />
+                    <h4>이 기 호</h4>
+                    <p class="text-muted">Back End</p>                    
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="team-member">
+                    <img class="p-2 mx-auto rounded-circle" src= {temp} alt="..." />
+                    <h4>이 동 현</h4>
+                    <p class="text-muted">Frond End</p>                    
+                </div>
+            </div>
+        </div>
+        
+    </div>    
     );
   }
 
--- FE/src/page/main.js
+++ FE/src/page/main.js
@@ -1,25 +1,36 @@
 import { useState } from 'react';
-import {Tab, Tabs} from 'react-bootstrap'
+import {Tab, Tabs} from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
-import main_img from './img/main_img.jpg';
+import main_img from './img/main_img.png';
 import About from './about';
 import Mint from './mint';
 import Query from './query';
 import Buy from './buy';
 import Sell from './sell';
 
+
+function remove_img() {
+  var element = document.getElementById("noneDiv");
+  element.classList.add("d-none")
+}
+
+
+
 function Main({myAddress, showPopUp}){
     const [tab, setTab] = useState("Main")
+    
     return<div>
       <Container className="panel" >
         <Tabs
           activeKey={tab}
           onSelect={(k) => setTab(k)}
           className="mb-3"
+          id = "tabs_selection"          
+          justify
         >
-          <Tab eventKey="About"  title="About" onClick={()=>setTab("<About>")}>
+          <Tab eventKey="About"  title="About" onClick={()=>setTab("About")}> 
           </Tab>
-          <Tab eventKey="Mint"  title="Mint" onClick={()=>setTab("<Mint>")}>
+          <Tab eventKey="Mint"  title="Mint" onClick={()=>setTab("Mint")}>
           </Tab>
           <Tab eventKey="Query" title="Query" onClick={()=>setTab("Query")}>
           </Tab>
@@ -28,17 +39,18 @@ function Main({myAddress, showPopUp}){
           <Tab eventKey="Sell" title="Sell" onClick={()=>setTab("Sell")}>
           </Tab>
         </Tabs>
-        
-        {/* <img src={main_img} width={"100%"} className="main_img"/> */}
-        
-        
-
+        {tab=="About" ? <About myAddress={myAddress} showPopUp={showPopUp}/>:null}
         {tab=="Mint" ? <Mint myAddress={myAddress} showPopUp={showPopUp}/>:null}
         {tab=="Query" ? <Query myAddress={myAddress} showPopUp={showPopUp}/>:null}
         {tab=="Buy" ? <Buy myAddress={myAddress} showPopUp={showPopUp}/>:null}
         {tab=="Sell" ? <Sell myAddress={myAddress} showPopUp={showPopUp}/>:null}
+
+        <div id="noneDiv"><img src={main_img} width={"100%"} className="main_img " alt="Main pic"/></div>
+        
       </Container>
     </div>
 }
+
+
 
 export default Main;
